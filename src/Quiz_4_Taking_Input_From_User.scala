f5fede8fd3b13257dc9155cfbef5cbe2dc5d03aa
Shubham Chourasia <shubham070496@gmail.com>
2022-02-23 14:42:09 +0530

    Quiz_3 : Taking Input from User - use of readLine() method

---
+++ src/Quiz_4_Taking_Input_From_User.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn.readLine
+
+object Quiz_4_Taking_Input_From_User {
+  def main(args: Array[String]): Unit={
+
+    // Take input from user - using readLine() method
+    // readLine() returns string value
+
+    var num1 = readLine("Enter the value for num1: ")
+    var num2 = readLine("Enter the value for num2: ")
+
+    var sum = num1.toInt + num2.toInt
+    var product = num1.toInt * num2.toInt
+
+    println("The sum of " + num1 + " and " + num2 + " is " + sum)
+    println("The product of " + num1 + " and " + num2 + " is " + product)
+
+  }
+
+}
