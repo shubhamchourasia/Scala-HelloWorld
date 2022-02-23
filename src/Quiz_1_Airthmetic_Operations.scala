b5a5ccfc310bcedb2422bef63432d85a825bd5fc
Shubham Chourasia <shubham070496@gmail.com>
2022-02-23 11:56:04 +0530

    Quiz_1 : Declare Variable in Scala

---
+++ src/Quiz_1_Airthmetic_Operations.scala
@@ -0,0 +1,12 @@
+object Quiz_1_Airthmetic_Operations {
+  def main(args: Array[String]): Unit ={
+    //Variable declaration
+    var a : Int = 5;
+    var b : Int = 5;
+    var c : Int = 5;
+
+    var equation = (((a+b)/c)*a);
+
+    println("Value of Equation is : " +  equation);
+  }
+}
