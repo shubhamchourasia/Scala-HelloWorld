cb86e4ea632f01fe95da52155dd0dc2972fb74fc
Shubham Chourasia <shubham070496@gmail.com>
2022-02-23 12:25:50 +0530

    Quiz_2 : String Manipulation - use of length() and concat() method.

--- src/Quiz_2_String_Manipulation.scala
+++ src/Quiz_2_String_Manipulation.scala
@@ -3,10 +3,16 @@ object Quiz_2_String_Manipulation {
     var string_val_1 : String = "Tom"
     var string_val_2 : String = "Jerry"
 
+    //Approach 1 : Using length method
+
     var len_of_string_1 : Int = string_val_1.length()
     var len_of_string_2 : Int = string_val_2.length()
 
-    println("The total length of both strings combined is " + (len_of_string_1 + len_of_string_2))
+    println("Approach 1: The total length of both strings combined is " + (len_of_string_1 + len_of_string_2))
+
+    //Approach 2 : Using concat method
+
+    println("Approach 2: The total length of both strings combined is " + string_val_1.concat(string_val_2).length())
 
   }
 
