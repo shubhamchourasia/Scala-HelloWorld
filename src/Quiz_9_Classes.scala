b474342fd0e98afa1c4b5ac0e74820e91d7dad70
Shubham Chourasia <shubham070496@gmail.com>
2022-03-03 21:13:02 +0530

    Quiz_9 : Classes in Scala
    Conepts Used:
    
    1. Classes
    2. Constructor
    3. UDFs in Class
    4. new Keyword
    5. Object creation

---
+++ src/Quiz_9_Classes.scala
@@ -0,0 +1,27 @@
+object Quiz_9_Classes {
+  class Number(var value: Int){
+    def printValue(): Unit ={
+      print(value)
+    }
+
+    def sum(n: Number): Int ={
+      return value + n.value
+    }
+
+    def isGreater(n: Number):Boolean ={
+      if(n.value > value) return true
+      else return false
+    }
+  }
+
+  def main(args: Array[String]):Unit={
+    var n1 = new Number(10)
+    var n2 = new Number(20)
+
+    println(n1.value)
+    println(n1.sum(n2))
+    println(n1.isGreater(n2))
+    println(n2.isGreater(n1))
+  }
+
+}
